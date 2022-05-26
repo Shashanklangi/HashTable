6a656805d11038d822709d54eec051d6b9a6ad6a
Shashank <langishashank@gmail.com>
2022-05-26 19:36:57 +0530

    [Shashank]:To find frequency of word in a phrase.

--- HashTable/HashTable/Program.cs
+++ HashTable/HashTable/Program.cs
@@ -8,7 +8,7 @@ namespace HashTable
             //UC1:Find the frequency of word in sentence
             Console.WriteLine("Hello Word");
             LinkedHashMap<string, int> linkedHashMap = new LinkedHashMap<string, int>(5);
-            string sentence = "to be or not to be";
+            string sentence = "paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
             string[] words = sentence.ToLower().Split(" ");
             foreach(string word in words)
             {
@@ -20,7 +20,7 @@ namespace HashTable
                 else value += 1;
                 linkedHashMap.Add(word, value);
             }
-            int frequency = linkedHashMap.Get("to");
+            int frequency = linkedHashMap.Get("paranoid");
             Console.WriteLine(frequency);
         }
     }
