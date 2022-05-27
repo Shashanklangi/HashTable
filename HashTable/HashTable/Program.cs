ba6d0e8040ccb09fed14daab5c9ad3677f93aac3
Shashank <langishashank@gmail.com>
2022-05-27 13:12:13 +0530

    [Shashank]:Removing the word 'avoidable' from the phrase.

--- HashTable/HashTable/Program.cs
+++ HashTable/HashTable/Program.cs
@@ -1,27 +1,35 @@
-﻿
-namespace HashTable
+﻿using HashTable;
+
+namespace DataStructure
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //UC1:Find the frequency of word in sentence
-            Console.WriteLine("Welcome to the binary search tree");
-            LinkedHashMap<string, int> linkedHashMap = new LinkedHashMap<string, int>(5);
-            string sentence = "paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
-            string[] words = sentence.ToLower().Split(" ");
-            foreach(string word in words)
+            Console.WriteLine("Welcome to the Hash Table");
+            LinkedHashMap<string, int> LinkedHashMap = new LinkedHashMap<string, int>(5);
+            
+            string Paragraph = "paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            string[] letters = Paragraph.ToLower().Split(" ");
+            foreach (string word in letters)
             {
-                int value = linkedHashMap.Get(word);
+                int value = LinkedHashMap.Get(word);
                 if (value == default)
                 {
                     value = 1;
                 }
                 else value += 1;
-                linkedHashMap.Add(word, value);
+                LinkedHashMap.Add(word, value);
             }
-            int frequency = linkedHashMap.Get("paranoid");
+            int frequency = LinkedHashMap.Get("paranoid");
             Console.WriteLine(frequency);
+         
+            LinkedHashMap.Remove("avoidable");
+            int frequency1 = LinkedHashMap.Get("avoidable");
+
+            Console.WriteLine(frequency1);
+
+
         }
     }
 }
